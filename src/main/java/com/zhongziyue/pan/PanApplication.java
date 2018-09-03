d84a5c4d50be95f53b12a166b544221663f6615b
zhongziyue <1017908236@qq.com>
2018-09-03 17:37:52 +0800

    修复链接的相对关系，更改部署方式为war

--- src/main/java/com/zhongziyue/pan/PanApplication.java
+++ src/main/java/com/zhongziyue/pan/PanApplication.java
@@ -3,11 +3,18 @@ package com.zhongziyue.pan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @EnableAutoConfiguration(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 })
 @SpringBootApplication
-public class PanApplication {
+public class PanApplication extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(PanApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PanApplication.class, args);
